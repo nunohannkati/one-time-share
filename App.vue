cfb716b43c8e034cb34e8084adcf27044e604413
nunohannkati <nunohannkati@gmail.com>
2021-09-04 23:29:19 +0900

    2021/8/4

---
+++ App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="twoColumnContainer">
+      <Slider />
+      <Twitter />
+    </div>
+    <Broadcast />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Slider from "./components/Slider.vue";
+import Twitter from "./components/Twitter.vue";
+import Broadcast from "./components/Broadcast.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Slider,
+    Twitter,
+    Broadcast,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Yu Gothic Medium", "游ゴシック Medium", YuGothic, "游ゴシック体",
+    "ヒラギノ角ゴ Pro W3", sans-serif;
+  line-height: 1.7;
+  color: #432;
+  margin: 0;
+}
+
+.twoColumnContainer {
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 350px;
+    gap: 0px;
+    padding-top: 0px;
+    margin: 0px 0px 0px 0px;
+}
+</style>
---
+++ Broadcast.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="broadcastContainer">
+    <h3 class="broadcastTitle">配信中</h3>
+    <div class="broadcastArea">
+      <div class="niconico">準備中</div>
+      <div id="twitch" class="twitch"></div>
+      <div class="youtube">準備中</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Broadcast",
+  mounted() {
+    let player = new Twitch.Player("twitch", {
+      channel: "nunohannkati",
+      width: "100%",
+      height: "100%",
+    });
+    player.setVolume(0.0);
+    player.addEventListener(Twitch.Player.PAUSE, () => {
+      console.log("Player is paused!");
+    });
+  },
+};
+</script>
+
+<style scoped>
+.broadcastContainer {
+  background-color: #f67690;
+  border-radius: 5px;
+  margin: 2px 2px 2px 2px;
+}
+
+.broadcastTitle {
+  font-size: 20px;
+  margin: 0;
+  margin-left: 4px;
+}
+
+.broadcastArea {
+  height: 161px;
+  display: flex;
+  justify-content: space-around;
+  gap: 8px;
+}
+
+.niconico {
+  background-color: #ffffff;
+  width: 272px;
+  height: 153px;
+}
+
+.twitch {
+  background-color: #ffffff;
+  width: 272px;
+  height: 153px;
+}
+
+.youtube {
+  justify-self: end;
+  background-color: #ffffff;
+  width: 272px;
+  height: 153px;
+}
+</style>
